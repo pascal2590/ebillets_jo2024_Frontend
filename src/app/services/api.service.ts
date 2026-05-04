942074a76545690c91a50e8473f9312747a2252d
pascal2590 <155544942+pascal2590@users.noreply.github.com>
2026-05-04 11:32:55 +0200

    Modification suite au déploiement via Cloudflare

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
-    private baseUrl = 'http://192.168.1.196:5000/api'; // private baseUrl = 'https://localhost:5001/api'
+
+    private baseUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: true,
-    // Possibilité d'utiliser une URL relative pour que le protocole HTTPS du site soit automatiquement utilisé
-    apiUrl: '/api',  // Utilise une URL relative (pas d'IP ou de port fixe)
-    // ipServeur: '192.168.1.92', // Pas utilisé pour les appels API en prod
+    apiUrl: 'https://api.pascalmorelprojetstudijoparis.xyz/api',
     apiKey: 'clé-prod'
 };
